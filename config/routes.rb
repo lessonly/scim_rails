7d1b3064304b4ccb9d24cdafc110df8b27577545
Spencer Alan <hello@spenceralan.com>
2018-11-05 12:04:42 -0500

    add standard routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 ScimRails::Engine.routes.draw do
+  get    'scim/v2/Users',      action: :index,        controller: 'scim_users'
+  post   'scim/v2/Users',      action: :create,       controller: 'scim_users'
+  get    'scim/v2/Users/:id',  action: :show,         controller: 'scim_users'
+  put    'scim/v2/Users/:id',  action: :update,       controller: 'scim_users'
+  patch  'scim/v2/Users/:id',  action: :deprovision,  controller: 'scim_users'
 end
