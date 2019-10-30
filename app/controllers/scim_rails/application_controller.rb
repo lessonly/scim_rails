ab754656f928d6d9d9529a91d6dca1058cc0b0ab
Joshua Azemoh <joshazemoh@gmail.com>
2019-10-30 22:56:22 +0100

    Add OAuth bearer authentication

--- app/controllers/scim_rails/application_controller.rb
+++ app/controllers/scim_rails/application_controller.rb
@@ -9,14 +9,30 @@ module ScimRails
     private
 
     def authorize_request
-      authenticate_with_http_basic do |username, password|
+      send(authentication_strategy) do |username, password|
         authorization = AuthorizeApiRequest.new(
           searchable_attribute: username,
           authentication_attribute: password
         )
         @company = authorization.company
       end
-      raise  ScimRails::ExceptionHandler::InvalidCredentials if @company.blank?
+      raise ScimRails::ExceptionHandler::InvalidCredentials if @company.blank?
+    end
+
+    def authentication_strategy
+      if request.headers["Authorization"]&.include?("Bearer")
+        :authenticate_with_oauth_bearer
+      else
+        :authenticate_with_http_basic
+      end
+    end
+
+    def authenticate_with_oauth_bearer
+      token = request.headers["Authorization"].split(" ").last
+      payload = ScimRails::Encoder.decode(token).with_indifferent_access
+      searchable_attribute = payload[ScimRails.config.basic_auth_model_searchable_attribute]
+
+      yield searchable_attribute, token
     end
   end
 end
