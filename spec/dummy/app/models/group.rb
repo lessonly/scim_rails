fdd524965d9dea6ff72fc2e174c9770eca94baa8
Matthew Yang <mwyang99@gmail.com>
2020-04-08 10:49:42 -0700

    fix missing symbol

--- spec/dummy/app/models/group.rb
+++ spec/dummy/app/models/group.rb
@@ -1,4 +1,4 @@
 class Group < ActiveRecord::Base
-  has_many :users, through: groups_users
+  has_many :users, through: :groups_users
   has_many :groups_users
 end
--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   belongs_to :company
 
-  has_many :groups, through: groups_users
+  has_many :groups, through: :groups_users
   has_many :groups_users
 
   validates \
