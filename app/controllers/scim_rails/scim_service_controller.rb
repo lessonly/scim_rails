166b9b85d4728e65c885e817f733d51f88ab4d54
Matthew Yang <mwyang99@gmail.com>
2020-12-09 11:59:58 -0800

    fix after_scim_response

--- app/controllers/scim_rails/scim_service_controller.rb
+++ app/controllers/scim_rails/scim_service_controller.rb
@@ -3,7 +3,7 @@ module ScimRails
     def configuration
       ScimRails.config.before_scim_response.call(request.params) unless ScimRails.config.before_scim_response.nil?
 
-      ScimRails.config.after_scim_response.call(users, "RETRIEVED") unless ScimRails.config.after_scim_response.nil?
+      ScimRails.config.after_scim_response.call(ScimRails.config.config_schema, "RETRIEVED") unless ScimRails.config.after_scim_response.nil?
 
       json_scim_response(object: nil)
     end
