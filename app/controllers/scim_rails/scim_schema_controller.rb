0a4213a005b1dcefa1ce1ffc86a7ff91d8b66d2a
Matthew Yang <mwyang99@gmail.com>
2020-12-09 16:05:33 -0800

    append params id and format

--- app/controllers/scim_rails/scim_schema_controller.rb
+++ app/controllers/scim_rails/scim_schema_controller.rb
@@ -3,9 +3,11 @@ module ScimRails
     def get_schema
       ScimRails.config.before_scim_response.call(request.params) unless ScimRails.config.before_scim_response.nil?
 
-      if request.params[:id] == "urn:ietf:params:scim:schemas:core:2.0:User"
+      id = request.params.key?("format") ? "#{request.params[:id]}.#{request.params[:format]}" : request.params[:id]
+
+      if id == "urn:ietf:params:scim:schemas:core:2.0:User"
         object = ScimRails.config.retrievable_user_schema
-      elsif request.params[:id] == "urn:ietf:params:scim:schemas:core:2.0:Group"
+      elsif id == "urn:ietf:params:scim:schemas:core:2.0:Group"
         object = ScimRails.config.retrievable_group_schema
       else
         object = {}
