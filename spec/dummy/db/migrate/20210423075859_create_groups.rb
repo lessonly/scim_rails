a42c69d6e61c64b06d70404d45c70fef1bf68338
Vale <valerauko@gmail.com>
2021-04-23 19:00:36 +0900

    Fake old migration version for rails 5 CI

--- spec/dummy/db/migrate/20210423075859_create_groups.rb
+++ spec/dummy/db/migrate/20210423075859_create_groups.rb
@@ -1,4 +1,4 @@
-class CreateGroups < ActiveRecord::Migration[6.1]
+class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
       t.string :name, null: false
--- spec/dummy/db/migrate/20210423075950_create_group_users.rb
+++ spec/dummy/db/migrate/20210423075950_create_group_users.rb
@@ -1,4 +1,4 @@
-class CreateGroupUsers < ActiveRecord::Migration[6.1]
+class CreateGroupUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :group_users do |t|
       t.references :group, null: false, foreign_key: true
